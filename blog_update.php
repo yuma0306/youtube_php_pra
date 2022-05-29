4c64bfd306cede33fa48fa886390a8cc47ea1017
yuma0306 <yuma0306895@gmail.com>
2022-05-29 21:43:52 +0900

    l11完了

--- blog_update.php
+++ blog_update.php
@@ -1,8 +1,8 @@
 <?php
+var_dump($_POST);
 require_once('blog.php');
 $blogs = $_POST;
-
 $blog = new Blog();
 $blog->blogValidate($blogs);
-$blog->blogCreate($blogs);
+$blog->blogUpdate($blogs);
 ?>
